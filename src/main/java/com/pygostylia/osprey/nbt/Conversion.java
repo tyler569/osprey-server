c4e0c9590357f9921bd8d369fe4044bde83a2cd6
Tyler Philbrick <t5j6p9@gmail.com>
2021-06-06 12:25:17 -0700

    Set NBT.Conversion methods as synchronized for now
    These need to be seperated so they don't have an unsynchronized shared buffer.

--- src/main/java/com/pygostylia/osprey/nbt/Conversion.java
+++ src/main/java/com/pygostylia/osprey/nbt/Conversion.java
@@ -9,43 +9,43 @@ import java.nio.ByteOrder;
 public class Conversion {
     static ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES).order(ByteOrder.BIG_ENDIAN);
 
-    public static void outputShort(OutputStream os, short v) throws IOException {
+    public static synchronized void outputShort(OutputStream os, short v) throws IOException {
         buffer.putShort(v);
         os.write(buffer.array(), 0, Short.BYTES);
         buffer.rewind();
     }
 
-    public static void outputShort(OutputStream os, int v) throws IOException {
+    public static synchronized void outputShort(OutputStream os, int v) throws IOException {
         buffer.putShort((short)v);
         os.write(buffer.array(), 0, Short.BYTES);
         buffer.rewind();
     }
 
-    public static void outputInteger(OutputStream os, int v) throws IOException {
+    public static synchronized void outputInteger(OutputStream os, int v) throws IOException {
         buffer.putInt(v);
         os.write(buffer.array(), 0, Integer.BYTES);
         buffer.rewind();
     }
 
-    public static void outputLong(OutputStream os, long v) throws IOException {
+    public static synchronized void outputLong(OutputStream os, long v) throws IOException {
         buffer.putLong(v);
         os.write(buffer.array(), 0, Long.BYTES);
         buffer.rewind();
     }
 
-    public static void outputFloat(OutputStream os, float v) throws IOException {
+    public static synchronized void outputFloat(OutputStream os, float v) throws IOException {
         buffer.putFloat(v);
         os.write(buffer.array(), 0, Float.BYTES);
         buffer.rewind();
     }
 
-    public static void outputDouble(OutputStream os, double v) throws IOException {
+    public static synchronized void outputDouble(OutputStream os, double v) throws IOException {
         buffer.putDouble(v);
         os.write(buffer.array(), 0, Double.BYTES);
         buffer.rewind();
     }
 
-    public static void putModifiedString(OutputStream os, String str) throws IOException {
+    public static synchronized void putModifiedString(OutputStream os, String str) throws IOException {
         DataOutputStream dso = new DataOutputStream(os);
         dso.writeUTF(str);
         dso.close();
