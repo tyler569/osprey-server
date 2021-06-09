f43d3f12f85c5f93a48248bd5a9992c1a63546eb
Tyler Philbrick <t5j6p9@gmail.com>
2021-06-08 23:18:10 -0700

    Improve BlockState to allow it to parse palette string states

--- src/main/java/com/pygostylia/osprey/BlockState.java
+++ src/main/java/com/pygostylia/osprey/BlockState.java
@@ -4,19 +4,90 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BlockState {
     private long state;
 
-    public BlockState(JSONObject registryObject) {}
-    public BlockState(int blockId, HashMap<String, Integer> states) {}
-    public BlockState(String blockIdString) {}
+    public BlockState(String blockName, JSONObject registryState) {
+        var blockId = blockIds.get(blockName);
+        if (blockId == null) {
+            throw new IllegalArgumentException("Illegal block name");
+        }
+        var blockType = blockTypes.get(blockId);
+        var fields = typeFields.get(blockType);
+
+        long internal = 0;
+        internal |= blockId;
+        internal |= (long) blockType.type() << blockIdLength;
+        var properties = registryState.optJSONObject("properties");
+        if (properties != null) {
+            for (var prop : properties.keySet()) {
+                var field = fields.get(prop);
+                var propValue = properties.getString(prop);
+                var propInt = field.values().get(propValue);
+                internal |= (long) propInt << field.offset;
+            }
+        }
+        state = internal;
+    }
+
+    public BlockState(int blockId) {
+        state = internalLongs.get(blockDefaultStates.get(blockId));
+    }
+
+    public BlockState(int blockId, HashMap<String, String> states) {
+        this(blockId);
+        for (var state : states.entrySet()) {
+            set(state.getKey(), state.getValue());
+        }
+    }
+
+    private static final Pattern statePattern = Pattern.compile("([^\\[]+)(?:\\[([^]]*)])?");
+
+    public BlockState(String blockIdString) {
+        Matcher m = statePattern.matcher(blockIdString);
+        String blockName;
+        String props;
+        if (m.find()) {
+            blockName = m.group(1);
+            props = m.group(2);
+        } else {
+            throw new IllegalArgumentException("Invalid block state pattern");
+        }
+        var blockId = blockIds.get(blockName);
+        if (blockId == null) {
+            throw new IllegalArgumentException("Invalid block type");
+        }
+        state = internalLongs.get(blockDefaultStates.get(blockId));
+        if (props != null) {
+            for (String prop : props.split(",")) {
+                String[] kv = prop.split("=", 2);
+                if (kv.length != 2) {
+                    throw new IllegalArgumentException("Invalid block prop format");
+                }
+                boolean success = set(kv[0].toLowerCase(), kv[1].toLowerCase());
+                if (!success) {
+                    throw new IllegalArgumentException(String.format("Invalid prop '%s' for block '%s'", kv[0], blockName));
+                }
+            }
+        }
+    }
+
+    public BlockState(InputStream stream) {
+        // consumes one state from the stream
+    }
+
+    public BlockState(short protocolId) {
+        state = internalLongs.get(protocolId);
+    }
 
     public BlockState(long value) {
         state = value;
@@ -26,9 +97,6 @@ public class BlockState {
     public String toString() {
         var sb = new StringBuilder();
         var blockId = blockId();
-        if (blockName() == null) {
-            throw new IllegalStateException("Oops");
-        }
         sb.append(blockName());
         if (fields() != null) {
             if (fields().size() > 0) {
@@ -84,7 +152,12 @@ public class BlockState {
     }
 
     public boolean set(String key, String value) {
-        return false;
+        var bitFieldDescription = fields().get(key);
+        if (bitFieldDescription == null) {
+            return false;
+        }
+        int intValue = bitFieldDescription.values.get(value);
+        return setRaw(key, intValue);
     }
 
     public Integer getRaw(String key) {
@@ -97,8 +170,17 @@ public class BlockState {
         return (int) value;
     }
 
-    public boolean setRaw(String key, Integer value) {
-        return false;
+    public boolean setRaw(String key, int value) {
+        var bitFieldDescription = fields().get(key);
+        if (bitFieldDescription == null) {
+            return false;
+        }
+        if (log2(value) > bitFieldDescription.width) {
+            throw new IllegalArgumentException(String.format("Value %d is too large for field '%s'", value, key));
+        }
+        state &= ~bitFieldDescription.mask();
+        state |= (long) value << bitFieldDescription.offset;
+        return true;
     }
 
     private Type type() {
@@ -120,8 +202,14 @@ public class BlockState {
     private static final Map<Type, Map<String, BitField>> typeFields = new HashMap<>();
     private static final Map<Short, Long> internalLongs = new HashMap<>();
     private static final Map<Long, Short> protocolIds = new HashMap<>();
+    private static final Map<Integer, Short> blockDefaultStates = new HashMap<>();
 
-    private static record Type(int type) {}
+    private static final int blockIdLength = 10;
+    private static final int typeIdLength = 8;
+
+    private static record Type(int type) {
+    }
+
     private static record BitField(int offset, int width, Map<String, Integer> values) {
         int maskAtZero() {
             return (1 << width) - 1;
@@ -135,7 +223,42 @@ public class BlockState {
     private static int nextType;
 
     static int log2(int v) {
+        if (v == 0) return 0;
         return 32 - Integer.numberOfLeadingZeros(v - 1);
+    }
+
+    private static Map<String, BitField> generatePropFields(JSONObject properties) {
+        var result = new HashMap<String, BitField>();
+        int offset = blockIdLength + typeIdLength;
+        if (properties != null) {
+            for (var prop : properties.keySet()) {
+                var count = properties.getJSONArray(prop).length();
+                var bits = log2(count);
+                var values = new HashMap<String, Integer>();
+                int valueN = 0;
+                for (var valuesObj : properties.getJSONArray(prop)) {
+                    String value = (String) valuesObj;
+                    values.put(value, valueN++);
+                }
+                result.put(prop, new BitField(offset, bits, values));
+                offset += bits;
+                if (offset > 63) {
+                    throw new IllegalStateException("Ran out of bits to encode block state");
+                }
+            }
+        }
+        return result;
+    }
+
+    private static Type findOrCreateType(Map<String, BitField> propFields) {
+        if (blockStateTypes.containsKey(propFields)) {
+            return blockStateTypes.get(propFields);
+        } else {
+            var newType = new Type(nextType++);
+            blockStateTypes.put(propFields, newType);
+            typeFields.put(newType, propFields);
+            return newType;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -152,59 +275,25 @@ public class BlockState {
         System.out.println(blockNames);
 
         for (var key : blocks.keySet()) {
-            final int blockIdLength = 10;
-            final int typeIdLength = 8;
             JSONObject block = blocks.getJSONObject(key);
-            var props = block.optJSONObject("properties");
-
-            int offset = blockIdLength + typeIdLength;
-            final var thisTypeFields = new HashMap<String, BitField>();
-            Type thisType;
-            if (props != null) {
-                for (var prop : props.keySet()) {
-                    var count = props.getJSONArray(prop).length();
-                    var bits = log2(count);
-                    var values = new HashMap<String, Integer>();
-                    int valueN = 0;
-                    for (var ovalue : props.getJSONArray(prop)) {
-                        String value = (String) ovalue;
-                        values.put(value, valueN++);
-                    }
-                    thisTypeFields.put(prop, new BitField(offset, bits, values));
-                    offset += bits;
-                    if (offset > 63) System.out.println("uh oh");
-                }
-            }
-            if (blockStateTypes.containsKey(thisTypeFields)) {
-                thisType = blockStateTypes.get(thisTypeFields);
-            } else {
-                thisType = new Type(nextType++);
-                blockStateTypes.put(thisTypeFields, thisType);
-                typeFields.put(thisType, thisTypeFields);
-            }
-            var blockId = blockIds.get(key);
+            final var props = block.optJSONObject("properties");
+            final var thisTypeFields = generatePropFields(props);
+            final Type thisType = findOrCreateType(thisTypeFields);
+            final var blockId = blockIds.get(key);
             blockTypes.put(blockId, thisType);
             var blockType = blockTypes.get(blockId);
             var fields = typeFields.get(blockType);
 
             JSONArray states = block.optJSONArray("states");
-            for (Object ostate : states) {
-                var state = (JSONObject) ostate;
-                long internal = 0;
-                internal |= blockId;
-                internal |= (long) blockType.type() << blockIdLength;
-                short stateId = (short) state.getInt("id");
-                var properties = state.optJSONObject("properties");
-                if (properties != null) {
-                    for (var prop : properties.keySet()) {
-                        var field = fields.get(prop);
-                        var propValue = properties.getString(prop);
-                        var propInt = field.values().get(propValue);
-                        internal |= (long) propInt << field.offset;
-                    }
+            for (Object map : states) {
+                var registryState = (JSONObject) map;
+                var blockState = new BlockState(key, registryState);
+                short stateId = (short) registryState.getInt("id");
+                internalLongs.put(stateId, blockState.state);
+                protocolIds.put(blockState.state, stateId);
+                if (registryState.optBoolean("default")) {
+                    blockDefaultStates.put(blockId, stateId);
                 }
-                internalLongs.put(stateId, internal);
-                protocolIds.put(internal, stateId);
             }
         }
         // System.out.println(blockStateTypes);
@@ -212,15 +301,21 @@ public class BlockState {
         // System.out.println(blockTypes);
         // System.out.println(internalLongs);
 
-        for (short protocolId = 0; protocolId < 1000; protocolId++) {
-            var longValue = internalLongs.get(protocolId);
-            var state = new BlockState(internalLongs.get(protocolId));
-            System.out.printf("%s %016x %s%n", state.protocolId(), longValue, state);
+        for (short protocolId = 0; protocolId < 20; protocolId++) {
+            var state = new BlockState(protocolId);
+            System.out.printf("%s %016x %s%n", state.protocolId(), state.longValue(), state);
         }
 
-        for (var x : protocolIds.keySet().stream().sorted().collect(Collectors.toList())) {
-            var state = new BlockState(x);
-            System.out.printf("%016x %d %s %s%n", x, Long.numberOfLeadingZeros(x), state, state.fields());
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String stateString = input.nextLine();
+            try {
+                var blockState = new BlockState(stateString);
+                System.out.printf("%016x %d %s%n", blockState.longValue(), blockState.protocolId(), blockState);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
